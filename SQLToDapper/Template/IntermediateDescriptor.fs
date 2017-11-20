7640b6d11b08ac4242031b2d2e5ab9b7a76f49e1
Jon Nyman <jon@selfcare.info>
2017-11-20 16:39:13 -0700

    intermediate type

---
+++ SQLToDapper/Template/IntermediateDescriptor.fs
@@ -0,0 +1,79 @@
+﻿namespace SQLToDapper
+
+module IntermediateDescriptor =
+
+    type Property =
+        {
+            name: string
+            ``type``: string
+            order: int
+            isNullable: bool
+        }
+
+    type MethodReturnSimple = string
+
+    type MethodReturnTable =
+        {
+            objectName: string
+            properties: Property list
+        }
+
+    type MethodReturn =
+        | MethodReturnSimple of MethodReturnSimple
+        | MethodReturnTable of MethodReturnTable
+
+    type ReturnType =
+        {
+            methodIDs: int list
+            returnTypes: MethodReturn
+        }
+
+    type UserDefinedType =
+        {
+            id: int
+            schema: string
+            name: string
+            properties: Property list
+        }
+
+    type Parameter =
+        {
+            /// Only used for UDT types
+            id: int option
+            name: string
+            ``type``: string
+            maxLength: int
+        }
+
+    type MethodSignature =
+        {
+            id: int
+            name: string
+            parameters: Parameter list
+        }
+
+    type ClassDescription =
+        {
+            methodSignatures: MethodSignature list
+            userDefinedTypes: UserDefinedType list
+            returnTypes: ReturnType list
+        }
+
+    type Schema =
+        {
+            /// Schema name
+            name: string
+            /// Stored procedures to explicitly include. Will not include other stored procedures
+            ``include``: string list option
+            /// Stored procedures to to exclude. If include has values this will be ingored.
+            exclude: string list option
+        }
+
+    type Intermediate =
+        {
+            ``namespace``: string
+            className: string
+            schemas: Schema list
+            classDescription: ClassDescription
+        }
+
