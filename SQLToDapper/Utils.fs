5b8c8d52645abf642e6bbc83905c8b6ddaf740c8
Jon Nyman <jon@selfcare.info>
2017-11-20 16:41:20 -0700

    no need for Option.orElse since using F# 4.1 now.

--- SQLToDapper/Utils.fs
+++ SQLToDapper/Utils.fs
@@ -5,9 +5,3 @@ module String =
     let capitalize (s : string) =
         let first = s.[0].ToString().ToUpper()
         first + s.[1..]
-
-module Option =
-    
-    let orElse ``default`` = function
-        | Some x -> x
-        | None -> ``default``
